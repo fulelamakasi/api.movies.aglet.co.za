100e7d3971da60f1966bf179fad7114db3dc9f73
Fulela Makasi <fulela.makasi@gmail.com>
2026-03-15 20:08:55 +0200

    added more tests

--- tests/test_contact_us_positive.py
+++ tests/test_contact_us_positive.py
@@ -1,8 +1,6 @@
 import requests
 
-
-def test_create_contact(client, headers):
-
+def test_create_contact_us(client, headers):
     payload = {
         "name": "John Doe",
         "email": "john@test.com",
@@ -18,3 +16,61 @@ def test_create_contact(client, headers):
     )
 
     assert response.status_code == 200
+
+def test_get_non_actioned_contact_us(client, headers):
+    response = requests.get(
+        f"{client}/language/get-active/v1/0",
+        headers=headers
+    )
+
+    assert response.status_code == 200
+
+def test_get_all_contact_us(client, headers):
+    response = requests.get(
+        f"{client}/contact_us/v1",
+        headers=headers
+    )
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_contact_us_by_id(client, headers):
+    contact_us_id = 1
+
+    response = requests.get(
+        f"{client}/contact_us/v1/{contact_us_id}",
+        headers=headers
+    )
+
+    assert response.status_code in [200, 404]
+
+def test_get_actioned_contact_us(client, headers):
+    response = requests.get(
+        f"{client}/language/get-active/v1/1",
+        headers=headers
+    )
+
+    assert response.status_code == 200
+
+def test_update_contact_us(client, headers):
+    contact_us_id = 2
+
+    payload = {
+        "id": 2,
+        "name": "John Doe",
+        "message": "I am enquiring on an inquiry about the inquiry of inquiries",
+        "email": "john@aglet.co.za",
+        "phone_number": "0797666321",
+        "company_name": "Swift Operators",
+        "is_actioned": 0,
+        "created_at": "2026-03-15 19:00:00",
+        "updated_at": ""
+    }
+
+    response = requests.put(
+        f"{client}/contact_us/v1/{contact_us_id}",
+        headers=headers,
+        json=payload
+    )
+
+    assert response.status_code in [200, 204]
--- tests/test_role_permissions_positive.py
+++ tests/test_role_permissions_positive.py
@@ -1,5 +1,18 @@
 import requests
 
+def test_create_role_permission(client, headers):
+    payload = {
+        "role_id": 1,
+        "permission_id": 1
+    }
+
+    response = requests.post(
+        f"{client}/role_permissions/v1",
+        headers=headers,
+        json=payload
+    )
+
+    assert response.status_code == 200
 
 def test_get_active_role_permissions(client, headers):
     response = requests.get(
@@ -8,3 +21,51 @@ def test_get_active_role_permissions(client, headers):
     )
 
     assert response.status_code == 200
+
+def test_get_all_role_permissions(client, headers):
+    response = requests.get(
+        f"{client}/role_permissions/v1",
+        headers=headers
+    )
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_role_permission_by_id(client, headers):
+    role_permission_id = 1
+
+    response = requests.get(
+        f"{client}/role_permissions/v1/{role_permission_id}",
+        headers=headers
+    )
+
+    assert response.status_code in [200, 404]
+
+def test_get_inactive_role_permissions(client, headers):
+    response = requests.get(
+        f"{client}/role_permissions/get-active/v1/0",
+        headers=headers
+    )
+
+    assert response.status_code == 200
+
+def test_update_role_permission(client, headers):
+    role_permission_id = 2
+
+    payload = {
+        "id": 2,
+        "role_id": 10,
+        "permission_id": 16,
+        "is_active": 1,
+        "is_deleted": 0,
+        "created_at": "2020-03-26 19:00:00",
+        "updated_at": ""
+    }
+
+    response = requests.put(
+        f"{client}/role_permissions/v1/{role_permission_id}",
+        headers=headers,
+        json=payload
+    )
+
+    assert response.status_code in [200, 204]
